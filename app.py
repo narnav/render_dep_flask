4a781d9a65bee4ff48e74e0ec7507e7bbe9a0a3e
narnavg <narnavg@gmail.com>
2022-11-14 18:17:09 +0200

    sce

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+ar=[{"name":"aviel"},{"name":"jacov"}]
 
 @app.route('/')
 def index():
     return 'Hello, World!'
 
 
-@app.route('/eli')
-def eli():
-    return 'Hello, eli!'
+@app.route('/dt')
+def dt():
+    return ar
 
 
 if __name__ == '__main__':
